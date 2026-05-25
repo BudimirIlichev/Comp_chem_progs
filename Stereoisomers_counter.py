5cc99c0a32a64c231ab857fc5580fae9bc04d185
BudimirIlichev <budimirilicev434@gmail.com>
2026-05-25 14:59:46 -0400

    Add files via upload

---
+++ Stereoisomers_counter.py
@@ -0,0 +1,16 @@
+from rdkit import Chem
+mol=Chem.MolFromSmiles("CC(N)(Cl)C(O)C(=O)O")
+if mol is None:
+    print("Invalid SMILES string")
+else: 
+ s=0
+for atom in mol.GetAtoms():
+    if atom.GetSymbol() == 'C':
+           neighbors = atom.GetNeighbors()
+    subs=[n for n in neighbors if n.GetSymbol() != 'H']
+    if len(subs)==4:
+            print("Chiral molecule")
+            s+=1
+print(f"Number of chiral centers: {s}")
+Chir_Isoms=2**s
+print(f"Number of stereoisomers: {Chir_Isoms}")
